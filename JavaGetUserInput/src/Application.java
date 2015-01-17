1ec04560f502b2448f19cbcbd22bff0d4e8330a8
jude <jude@affle.com>
2015-01-18 00:58:54 +0800

    placed indentation

--- JavaGetUserInput/src/Application.java
+++ JavaGetUserInput/src/Application.java
@@ -7,10 +7,13 @@ public class Application
         {
         //create scanner object
         Scanner input = new Scanner(System.in);
+
         //output the prompt
         System.out.println("Enter a line of text : ");
+
         //waiting for the user to enter a line of text
         String line = input.nextLine();
+
         //tell them what they entered
         System.out.println("You Entered  :  " + line);
 
