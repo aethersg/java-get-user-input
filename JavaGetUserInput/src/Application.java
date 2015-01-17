d67664e528de23dcd634ddaa044ef8364ab73fca
jude <jude@affle.com>
2015-01-18 00:49:05 +0800

    initial commit

---
+++ JavaGetUserInput/src/Application.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+
+public class Application
+    {
+    public static void main(String[] args)
+        {
+        //create scanner object
+        Scanner input = new Scanner(System.in);
+        //output the prompt
+        System.out.println("Enter a line of text : ");
+        //waiting for the user to enter a line of text
+        String line = input.nextLine();
+        //tell them what they entered
+        System.out.println("You Entered  :  " + line);
+
+        }
+    }
